197f528b7d101888665afebb8b37749c502aacb4
KTRMAmbiance <100489366+KTRMAmbiance@users.noreply.github.com>
2026-07-29 22:24:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 window.onload = function() {
     const currenttime = new Date();
     if (currenttime.getHours() >= 22 || currenttime.getHours() < 7) {
-        document.body.style.backgroundImage = "url('_bg/night_full.png')";
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/KTRMAmbiance/ktrmambiance.github.io/refs/heads/main/_bg/night_full.png')";
     }
     else if (currenttime.getHours() >= 18) {
-        document.body.style.backgroundImage = "url('_bg/sunset_full.png')";
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/KTRMAmbiance/ktrmambiance.github.io/refs/heads/main/_bg/sunset_full.png')";
     }
     else if (currenttime.getHours() >= 13) {
-        document.body.style.backgroundImage = "url('_bg/daytime_full.png')";
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/KTRMAmbiance/ktrmambiance.github.io/refs/heads/main/_bg/daytime_full.png')";
     }
     else {
-        document.body.style.backgroundImage = "url('_bg/morning_full.png')";
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/KTRMAmbiance/ktrmambiance.github.io/refs/heads/main/_bg/morning_full.png')";
     }
 }
 
